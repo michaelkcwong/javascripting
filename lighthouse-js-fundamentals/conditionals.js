0c584864f64e116f94e30b4d00e7355f8b669b9d
michaelkcwong <wong_michael@rogers.com>
2021-01-09 21:50:19 +0000

    Add more conditional examples

--- lighthouse-js-fundamentals/conditionals.js
+++ lighthouse-js-fundamentals/conditionals.js
@@ -1,5 +1,5 @@
 //Temperature conditional example
-const temperature = 30;
+var temperature = 30;
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
@@ -10,3 +10,23 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+//Checking if citizen is eligible to vote with logical operators (&&)
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+//Temperature check with logical operators(||)
+var temperature = 50
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+//Raining example with logical operator (!)
+var raining = false
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
